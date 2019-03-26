88b8492d88baeb8c2633512159e8a4af263fc1e4
Sungje Kim <sungje.kim@one-line.com>
2019-03-26 20:58:23 +0900

    Test 007

--- MMM-videoPlay.js
+++ MMM-videoPlay.js
@@ -4,20 +4,7 @@ Module.register("MMM-videoPlay", {
         videoAddrDom: 'http://localhost/',
         videoArray: ['ONE1.mp4', 'ONE2.mp4', 'ONE3.mp4'],
         loop: true,
-        updateInterval: 0 //reads the file every 30 mins
-    },
 
-    start: function () {
-        this.sendSocketNotification("START", this.config);
-    },
-
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === "TIME") {
-            this.datafile = payload
-            this.config.videoNum = this.datafile[0];
-            this.config.updateInterval = this.datafile[1];
-            this.updateDom();
-        }
     },
 
     getStyles: function(){
@@ -26,21 +13,16 @@ Module.register("MMM-videoPlay", {
 
     getDom: function () {
         
-        var wrapper = document.createElement("div");
         var video = document.createElement("video");
-       
-        // video.loop = this.config.loop;
-        if (this.dataFile) {
-            video.src = this.config.videoAddrDom + this.config.videoArray[this.config.videoNum];
-            video.autoplay = true;
-            wrapper.appendChild(video);
 
+        video.src = this.config.videoAddrDom + this.config.videoArray[this.config.videoNum];
+        video.autoplay = true;
+        video.onended = function(){
+            this.config.videoNum += 1;
             if (this.config.videoNum >= 2) this.config.videoNum = 0;
-            else this.config.videoNum += 1;
-
-        } else {
-            wrapper.innerHTML = "No data";
+            video.src = this.config.videoAddrDom + this.config.videoArray[this.config.videoNum]
         }
-        return wrapper;
+        
+        return video;
     }
 });
--- node_helper.js
+++ node_helper.js
@@ -18,7 +18,21 @@ module.exports = NodeHelper.create({
     setTime: function (payload) {
         this.config = payload;
 
-        var tempVideo = window.document.createElement('video');
+        var buff = new Buffer(100);
+        fs.open(file, 'r', function (err, fd) {
+            fs.read(fd, buff, 0, 100, 0, function (err, bytesRead, buffer) {
+                var start = buffer.indexOf(new Buffer('mvhd')) + 17;
+                var timeScale = buffer.readUInt32BE(start, 4);
+                var duration = buffer.readUInt32BE(start + 4, 4);
+                var movieLength = Math.floor(duration / timeScale);
+
+                console.log('time scale: ' + timeScale);
+                console.log('duration: ' + duration);
+                console.log('movie length: ' + movieLength + ' seconds');
+            });
+        });
+
+        var tempVideo = document.createElement('video');
         tempVideo.src = this.config.videoAddrDom + this.config.videoArray[this.config.videoNum];
 
         this.config.updateInterval = tempVideo.duration + 2;
